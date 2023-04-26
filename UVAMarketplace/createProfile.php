65f2363564fac48d5505ffae610c2e4116c9c80b
Addison S Lowman <asl7xdp@portal03.cs.virginia.edu>
2023-04-26 13:34:06 -0400

    html for profile creation

--- UVAMarketplace/createProfile.php
+++ UVAMarketplace/createProfile.php
@@ -32,35 +32,97 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">   
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">  
-  <title>Create Profile</title> 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
-  <link rel="stylesheet" href="activity-styles.css" /> 
+	<title>Profile Registration Page</title>
+	<style>
+		body {
+			background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp'); /* Change the image path to your desired background image */
+			background-repeat: no-repeat;
+			background-size: cover;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100vh;
+			margin: 0;
+			padding: 0;
+			font-family: Arial, sans-serif;
+			font-size: 16px;
+		}
+
+		form {
+			background-color: #fff;
+			padding: 30px;
+			border-radius: 10px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			text-align: center;
+		}
+
+		h1 {
+			margin: 0 0 30px;
+			font-size: 32px;
+			font-weight: bold;
+		}
+
+		input,
+		select {
+			padding: 10px;
+			margin-bottom: 20px;
+			width: 100%;
+			box-sizing: border-box;
+			border: none;
+			border-radius: 5px;
+			box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+		}
+
+		textarea {
+			padding: 10px;
+			margin-bottom: 20px;
+			width: 100%;
+			box-sizing: border-box;
+			border: none;
+			border-radius: 5px;
+			box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+			resize: none;
+			height: 100px;
+		}
+
+		button {
+			padding: 10px 20px;
+			background-color: #333;
+			color: #fff;
+			border: none;
+			border-radius: 5px;
+			box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+			cursor: pointer;
+			font-size: 16px;
+		}
+
+		button:hover {
+			background-color: #444;
+		}
+	</style>
 </head>
-<body>  
-  <div>  
-    <h1>Create Profile</h1>
-    <form action="createProfile.php" method="post" enctype="multipart/form-data">     
-      Computing ID: <input type="text" name="computingID" minlength="6" maxlength="7" required /> <br/>
-      Name: <input type="text" name="name" required /> <br/>
-
-      <input type="radio" name="year" value="1" checked>
-      <label for="1">1</label><br>
-      <input type="radio" name="year" value="2">
-      <label for="2">2</label><br>
-      <input type="radio" name="year" value="3">
-      <label for="3">3</label><br>
-      <input type="radio" name="year" value="4">
-      <label for="4">4</label><br>
-
-      <input type="file" name="profilePic" accept="image/png, image/jpeg, image/jpg"> <br>
-      <input type="submit" name ="createProfileBtn" value="Submit" class="btn" /> <br/>
-    </form>
-  </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
+<body>
+	<form>
+		<h1>Profile Registration</h1>
+		<label for="computing_id">Computing ID:</label>
+		<input type="text" id="computing_id" name="computing_id" required>
+		<label for="name">Name:</label>
+		<input type="text" id="name" name="name" required>
+		<label for="username">Username:</label>
+		<input type="text" id="username" name="username" required>
+		<label for="year">Current Year in School:</label>
+		<select id="year" name="year">
+			<option value="1">1st Year</option>
+			<option value="2">2nd Year</option>
+			<option value="3">3rd Year</option>
+			<option value="4">4th Year</option>
+		</select>
+		<label for="profile_pic">Profile Picture:</label>
+		<input type="file" id="profile_pic" name="profile_pic" accept="image/*" required>
+		<button type="submit">Submit</button>
+	</form>
 </body>
 </html>
