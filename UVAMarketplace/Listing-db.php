22538a54a66c5e2ba805f3e7362a88c9d201a2a4
evan-yan <89653888+evan-yan@users.noreply.github.com>
2023-04-30 16:16:53 -0400

    Add files via upload

---
+++ UVAMarketplace/Listing-db.php
@@ -0,0 +1,170 @@
+<?php
+ini_set('error_reporting', -1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require('Clothes-db.php');
+require('Furniture-db.php');
+require('Books-db.php');
+
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+function getListingByID($listingID) {
+    global $db;
+
+    $query = "select * from Listing where listingID=:listingID";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':listingID', $listingID);
+    $statement->execute();
+    $results = $statement -> fetch();
+    $statement->closeCursor();
+    return $results;
+}
+
+function getListingsByUser($computingID) {
+    global $db;
+
+    $query = "select * from Listing where sellerID=:computingID order by post_date DESC";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':computingID', $computingID);
+    $statement->execute();
+    $results = $statement -> fetchALL();
+    $statement->closeCursor();
+    return $results;
+}
+
+function getFavoriteListings($computingID) {
+    global $db;
+
+    $query = "select * from Listing where exists (select * from favorites where buyerID=:buyerID and Listing.listingID=favorites.listingID)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':buyerID', $computingID);
+    $statement->execute();
+    $results = $statement -> fetchALL();
+    $statement->closeCursor();
+    return $results;
+}
+
+function getAllListings($orderBy) {
+    global $db;
+
+    //Clothes
+    if ($orderBy == "2") {
+        $query = "SELECT * FROM Listing WHERE listingID in (select Clothes.listingID from Clothes where Listing.listingID = Clothes.listingID)";
+    }
+    //Furniture
+    elseif ($orderBy == "1") {
+        $query = "SELECT * FROM Listing WHERE listingID in (select Furniture.listingID from Furniture where Listing.listingID = Furniture.listingID)";
+    }
+    //Textbook
+    elseif ($orderBy == "3") {
+        $query = "SELECT * FROM Listing WHERE listingID in (select Books.listingID from Books where Listing.listingID = Books.listingID)";
+    }
+    else{
+        $query = "SELECT * FROM Listing WHERE 
+            listingID in (select Clothes.listingID from Clothes where Listing.listingID = Clothes.listingID) or
+            listingID in (select Furniture.listingID from Furniture where Listing.listingID = Furniture.listingID) or
+            listingID in (select Books.listingID from Books where Listing.listingID = Books.listingID) ORDER BY $orderBy";
+    }
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $listings = $statement->fetchAll();
+    $statement->closeCursor();
+    return $listings;
+  }
+
+function deleteListing($listingID) {
+    global $db;
+
+    $listing = getListingByID($listingID);
+    $category = $listing['categoryID'];
+
+    //Clothes
+    if ($category == 2) {
+        deleteClothes($listingID);
+    }
+    //Furniture
+    elseif ($category == 1) {
+        deleteFurniture($listingID);
+    }
+    //Textbook
+    else {
+        deleteBook($listingID);
+    }
+
+    $query = "delete from Listing where listingID=:listingID";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':listingID', $listingID);
+    $statement->execute();
+    $statement->closeCursor();
+
+
+}
+
+function createListing($title, $location, $description, $category, $size, $material, $dimensions, $bookTitle, $course, $IBSN, $condition, $listed_price, $itemPic) {
+    global $db;
+    date_default_timezone_set('America/New_York');
+    $category = (int)$category;
+    $listed_price = (float)$listed_price; 
+
+    //Get last row of listing table
+    $query = "select listingID from Listing where listingID=(select max(listingID) from Listing);";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $results = $statement -> fetch();
+    $statement->closeCursor();
+    $listingID = (int)$results['listingID'] + 1;
+
+    $_SESSION['listingID'] = $listingID; //Used for view listing
+
+    if ($itemPic['name']) {
+        //Add profile pic under profilePics/username.jpg/png/jpeg
+        $imgExt = pathinfo($itemPic['name'], PATHINFO_EXTENSION);
+        $imgName = $listingID.'.'.$imgExt;
+        $tmpName = $itemPic['tmp_name'];
+        $uploadPath = '../itemPics/'.$imgName;
+        move_uploaded_file($tmpName, $uploadPath);
+    }
+    else {
+        $imgName = "defaultItem.jpeg";
+    }
+
+    //Add listing
+    $query = "insert into Listing values (:listingID, :title, :post_date, :location, :description, :itemPic, :condition, :listed_price, :sellerID, :categoryID)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':listingID', $listingID);
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':post_date', date('Y-m-d H:i:s'));
+    $statement->bindValue(':location', $location);
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':itemPic', $imgName);
+    $statement->bindValue(':condition', $condition);
+    $statement->bindValue(':listed_price', $listed_price);
+    $statement->bindValue(':sellerID', $_SESSION['computingID']);
+    $statement->bindValue(':categoryID', $category);
+    $statement->execute();
+    $statement->closeCursor();
+
+    //Clothes
+    if ($category == 2) {
+        //Add clothes entry
+        addClothes($category, $size, $listingID);
+    }
+    //Furniture
+    elseif ($category == 1) {
+        //Add furniture entry
+        addFurniture($category, $material, $dimensions, $listingID);
+    }
+    //Textbook
+    else {
+        //Add book entry
+        addBook($category, $bookTitle, $course, $IBSN, $listingID);
+    }
+}
+
+
+?>
---
+++ UVAMarketplace/viewListing.php
@@ -0,0 +1,131 @@
+<?php
+require("connect-db.php");
+require('Account-db.php');
+require('User-db.php');
+require('Listing-db.php');
+require('Offer-db.php');
+require('favorites-db.php');
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+if (!$_SESSION['username']) {
+    header("Location: signup.php");
+    exit();
+} 
+elseif(!isUser($_SESSION['username'])) {
+    header("Location: createProfile.php");
+    exit();
+}
+elseif (!$_SESSION) {
+    setSessionVars($_SESSION['username']);
+}
+elseif (!$_SESSION['listingID']) {
+    header("Location: home.php");
+    exit();
+}
+
+$listing = getListingByID($_SESSION['listingID']);
+$isMyListing = $listing['sellerID'] == $_SESSION['computingID'];
+if ($isMyListing) {
+    $offers = getOffersForListing($listing['listingID']);
+}
+else {
+    $isFavorite = isFavorite($_SESSION['computingID'], $listing['listingID']);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($_POST['makeOfferBtn'])) {
+        $_SESSION['listingID'] = $_POST['listingToOffer'];
+        header("Location: makeOffer.php");
+        exit();
+    }
+    elseif (!empty($_POST['favoriteBtn'])) {
+        favorite($_SESSION['computingID'], $listing['listingID']);
+        $isFavorite = isFavorite($_SESSION['computingID'], $listing['listingID']);
+    }
+    elseif (!empty($_POST['unfavoriteBtn'])) {
+        unfavorite($_SESSION['computingID'], $listing['listingID']);
+        $isFavorite = isFavorite($_SESSION['computingID'], $listing['listingID']);
+    }    
+    elseif (!empty($_POST['unfavoriteBtn'])) {
+        unfavorite($_SESSION['computingID'], $listing['listingID']);
+        $isFavorite = isFavorite($_SESSION['computingID'], $listing['listingID']);
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<?php include 'navbar.php';?>
+<head>
+  <meta charset="utf-8">   
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">  
+  <title>Home</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+  <link rel="stylesheet" href="activity-styles.css" /> 
+</head>
+<h1> <?php echo $listing['title']?></h1>
+<body>  
+<img style="max-width: 300px; height:auto; max-height: 200px;  margin-left: auto; margin-right: auto;" src="../itemPics/<?=$listing['itemPic']?>" > <br>
+Description: <?php echo $listing['description']?> <br>
+Date posted: <?php echo $listing['post_date']?> <br>
+Location: <?php echo $listing['location']?> <br>
+Condition: <?php echo $listing['condition']?> <br>
+Price: $<?php echo $listing['listed_price']?> <br>
+
+<?php if(!$isMyListing) : ?>
+    <form action="viewListing.php" method="post" >
+        <input type="submit" name="makeOfferBtn" value="Make Offer" class="btn btn-dark"/>
+        <input type="hidden" name="listingToOffer" value="<?php echo $listing['listingID'];?>" /> 
+    <?php if(!$isFavorite) : ?>
+            <input type="submit" name="favoriteBtn" value="Add to favorites" class="btn btn-warning"/>
+            <input type="hidden" name="listingToFavorite" value="<?php echo $listing['listingID'];?>" /> 
+    <?php else : ?>
+            <input type="submit" name="unfavoriteBtn" value="Unfavorite" class="btn btn-danger"/>
+    <?php endif; ?>
+    </form>
+
+<?php else : //is my listing?>
+    Offers: 
+    <div class="row justify-content-center">  
+    <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+      <thead>
+      <tr style="background-color:#B0B0B0">
+        <th> Buyer name </th> 
+        <th> Computing ID </th>
+        <th> Offer Price </th>
+        <th>Accept </th>
+        <th> Reject </th>
+      </tr>
+      </thead>
+    <?php foreach ($offers as $offer): ?>
+    <?php $buyer = getUser($offer['buyerID']); ?>
+      <tr>
+        <td><?php echo $buyer['name']; ?></td>  
+        <td><?php echo $buyer['computingID']; ?></td> 
+        <td>$<?php echo $offer['offer_price']; ?></td>  
+        <td> 
+          <form action="viewListing.php" method="post" >
+            <input type="submit" name="acceptOfferBtn" value="Accept" class="btn btn-success"/>
+            <input type="hidden" name="offerToAccept" value="<?php echo $offer['offerID'];?>" />     
+          </form>  
+        </td> 
+        <td> 
+          <form action="viewListing.php" method="post" >
+            <input type="submit" name="rejectOfferBtn" value="Reject" class="btn btn-danger"/>
+            <input type="hidden" name="offerToReject" value="<?php echo $offer['offerID'];?>" />     
+          </form>  
+        </td> 
+      </tr>
+    <?php endforeach; ?>
+</table>
+</div>
+<?php endif; ?>
+
+</body>
+</html>
