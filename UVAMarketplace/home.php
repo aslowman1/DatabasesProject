98386571f91f1741304343781cbb4f645eedfd27
Thumay Vu Huynh <tvh4xuk@portal03.cs.virginia.edu>
2023-04-30 21:52:13 -0400

    more changes

--- UVAMarketplace/home.php
+++ UVAMarketplace/home.php
@@ -60,10 +60,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <link rel="stylesheet" href="activity-styles.css" /> 
 
   <?php include 'navbar.php';?>
-  <div class="container-fluid">
+
+  <div class="container-fluid" style="padding-top: 20px">
   <div class="row justify-content-end">
-    <div class="col-4">
-      <button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#sortPanel" aria-expanded="false" aria-controls="sortPanel" style="float: right;">Sort Products</button>
+    <div class="col-4" style="padding-bottom: 10px">
+      <button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#sortPanel" aria-expanded="false" aria-controls="sortPanel" style="float: right; font-family: Lucida Console, Courier New, monospace; 
+      font-size: 15px; text-align: center; font-weight: bold ">Sort Products</button>
     </div>
   </div>
   <div class="collapse" id="sortPanel" style="">
@@ -93,7 +95,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       background-size: cover;
     }
     .card {
-      box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+      max-width: 400px;
+      margin: auto;
+      font-family: arial;
+      box-shadow: 5px 5px 10px 1px rgba(0, 0, 0, 0.6);
+      background-color: #EDDECF;
     }
     .button-group {
       display: flex;
@@ -102,33 +108,90 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     .button-group form {
       margin: 0 5px;
     }
+
+    .button{
+      align-items: center;
+      appearance: none;
+      background-color: #FCFCFD;
+      border-radius: 4px;
+      border-width: 0;
+      box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#D6D6E7 0 -3px 0 inset;
+      box-sizing: border-box;
+      color: #36395A;
+      cursor: pointer;
+      display: inline-flex;
+      font-family: "JetBrains Mono",monospace;
+      height: 30px;
+      justify-content: center;
+      line-height: 1;
+      list-style: none;
+      overflow: hidden;
+      padding-left: 16px;
+      padding-right: 16px;
+      position: relative;
+      text-align: left;
+      text-decoration: none;
+      transition: box-shadow .15s,transform .15s;
+      user-select: none;
+      -webkit-user-select: none;
+      touch-action: manipulation;
+      white-space: nowrap;
+      will-change: box-shadow,transform;
+      font-size: 15px;
+    }
+
+    .button:focus {
+      box-shadow: #D6D6E7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+    }
+
+    .button:hover {
+      box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+      transform: translateY(-2px);
+    }
+
+    .button:active {
+      box-shadow: #D6D6E7 0 3px 7px inset;
+      transform: translateY(2px);
+    }
+    h1{
+      font-weight: bold;
+      font-family: Tahoma, Geneva, sans-serif; 
+      font-size: 15px
+    }
+
   </style>
 </head>
 <body>
   <div class="container">
-    <div class="row row-cols-3 g-3">
+    <div class="row row-cols-4 g-3">
       <?php foreach ($listings as $listing): ?>
         <div class="col">
           <div class="card h-100" >
-            <img style="max-width: 300px; height:auto; max-height: 200px;  margin-left: auto; margin-right: auto;" src="../itemPics/<?=$listing['itemPic']?>" class="card-img-top"/>
+            <img style="max-width: 300px; height:auto; max-height: 200px;  margin-left: auto; margin-right: auto; padding-top: 5px" src="../itemPics/<?=$listing['itemPic']?>" class="card-img-top"/>
             <div class="card-body">
-              <h5 class="card-title"><?php echo $listing['title']; ?></h5>
-              <p class="card-text" >
-                  Seller: <?php echo getUser($listing['sellerID'])['name']; ?> <br>
-                  Price: $<?php echo $listing['listed_price']; ?> <br>
-                  Description: <?php echo $listing['description']; ?> <br>
-                  Location: <?php echo $listing['location']; ?>
-                  Post date: <?php echo $listing['post_date']; ?>
-              </p>
-              <div class="button-group">
-                <form action="home.php" method="post" >
-                  <input type="submit" name="viewListingBtn" value="View Listing" class="btn btn-dark"/>
-                  <input type="hidden" name="listingToView" value="<?php echo $listing['listingID'];?>" />     
-                </form> 
-                <form action="home.php" method="post" >
-                  <input type="submit" name="viewSellerBtn" value="View Seller" class="btn btn-dark"/>
-                  <input type="hidden" name="sellerID" value="<?php echo $listing['sellerID'];?>" />     
-                </form>
+              <h5 class="card-title" style="font-family: Lucida Console, Courier New, monospace; font-size: 20px; text-align: center;
+              font-weight: bold"><?php echo $listing['title']; ?></h5>
+              <div class="row">
+                <p class="card-text" style="text-align: left, font-size: 10px;  line-height: 0.8;" >
+                <div class="col-sm">
+                  <h1>Seller:</h1> <?php echo getUser($listing['sellerID'])['name']; ?> <br>
+                </div>
+                <div class="col-sm" style="text-align: right">
+                    <h1>Price:</h1> $<?php echo $listing['listed_price']; ?> <br>
+                    <h1>Location:</h1> <?php echo $listing['location']; ?>
+                    <h1>Post date:</h1> <?php echo $listing['post_date']; ?>
+                </div>
+                </p>
+                <div class="button-group">
+                  <form action="home.php" method="post" >
+                    <input type="submit" name="viewListingBtn" value="View Listing" class="button"/>
+                    <input type="hidden" name="listingToView" value="<?php echo $listing['listingID'];?>" />     
+                  </form> 
+                  <form action="home.php" method="post" >
+                    <input type="submit" name="viewSellerBtn" value="View Seller" class="button"/>
+                    <input type="hidden" name="sellerID" value="<?php echo $listing['sellerID'];?>" />     
+                  </form>
+                </div>
               </div>
             </div>
           </div>
